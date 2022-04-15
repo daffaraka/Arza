8d2fd7b74d3d5300744e9b37af289bd0bec29ffc
Satomo <satriyo@ymail.com>
2022-04-15 14:31:54 +0700

    update

--- aset-tetap/resources/views/Aset/Edit-Aset.blade.php
+++ aset-tetap/resources/views/Aset/Edit-Aset.blade.php
@@ -1,38 +1,62 @@
 @extends('dashboard.layouts.main')
 
 @section('container')
-<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-  <h1 class="h2">Aset Tetap</h1>
-</div>
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">Aset Tetap</h1>
+    </div>
 
-<h5 class="mt-3 mb-3"> Edit Aset Tetap</h5>
+    <h5 class="mt-3 mb-3"> Edit Aset Tetap</h5>
 
-  <form action="{{ url('Aset/update_aset', $aset->id) }}" method="POST">
-    
-    {{ csrf_field() }}
-    <div class="form-group mb-2">
-      <label for="number" class="col-sm-2 col-form-label">Kode Barang</label>
-      <input type="number" id="kode_barang" name="kode_barang" class="form-control" placeholder="Kode Barang">
-    </div>
-    <div class="form-group mb-2">
-      <label for="text" class="col-sm-2 col-form-label">Register</label>
-      <input type="text" id="register" name="register" class="form-control" placeholder="Register">
-    </div>
-    <div class="form-group mb-2">
-      <label for="text" class="col-sm-2 col-form-label">Nama Barang</label>
-      <input type="text" id="nama_barang" name="nama_barang" class="form-control" placeholder="Nama Barang">
-    </div>
-    <div class="form-group mb-2">
-      <label for="text" class="col-sm-2 col-form-label">Jumlah</label>
-      <input type="text" id="jumlah" name="jumlah" class="form-control" placeholder="Jumlah">
-    </div>
-    <div class="form-group mb-2">
-      <label for="text" class="col-sm-2 col-form-label">Harga</label>
-      <input type="text" id="harga" name="harga" class="form-control" placeholder="Harga">
-    </div>
-    <div class="form-group">
-      <button type="submit" class="btn btn-primary">Ubah</button>
-    </div>
-  </form>
+    <form action="{{ url('Aset/update_aset', $aset->id) }}" method="POST">
 
+        {{ csrf_field() }}
+        <div class="form-group mb-2">
+            <label for="number" class="col-sm-2 col-form-label">Kode Barang</label>
+            <select class="form-select" aria-label="Default select example" id="kode">
+                <option selected disabled>- Pilih Kode -</option>
+                <option id="select_1,01" value="1,01 | Tanah Kota">1,01</option>
+                <option id="select_1,02" value="1,02 | Alat Berat">1,02</option>
+                <option id="select_1,03" value="1,03 | Baju">1,03</option>
+            </select>
+            <input type="text" id="kode_barang" name="kode_barang" class="d-none" placeholder="Kode Barang" readonly>
+        </div>
+        <div class="form-group mb-2">
+            <label for="text" class="col-sm-2 col-form-label">Register</label>
+            <input type="text" id="register" name="register" class="form-control" placeholder="Register">
+        </div>
+        <div class="form-group mb-2">
+            <label for="text" class="col-sm-2 col-form-label">Nama Barang</label>
+            <input type="text" id="nama_barang" name="nama_barang" class="form-control" placeholder="Nama Barang"
+                readonly>
+        </div>
+        <div class="form-group mb-2">
+            <label for="text" class="col-sm-2 col-form-label">Jumlah</label>
+            <input type="text" id="jumlah" name="jumlah" class="form-control" placeholder="Jumlah">
+        </div>
+        <div class="form-group mb-2">
+            <label for="text" class="col-sm-2 col-form-label">Harga</label>
+            <input type="text" id="harga" name="harga" class="form-control" placeholder="Harga">
+        </div>
+        <div class="form-group">
+            <button type="submit" class="btn btn-primary">Ubah</button>
+        </div>
+
+        <script>
+            var kode = '1,03'
+            var nama = 'Baju'
+            var select = document.getElementById("kode");
+            var selectedItem = document.getElementById("select_" + kode);
+            var inputKodeBarang = document.getElementById("kode_barang");
+            var inputBarang = document.getElementById("nama_barang");
+
+            select.addEventListener("change", function() {
+                inputKodeBarang.value = select.value.split(" | ")[0];
+                inputBarang.value = select.value.split(" | ")[1];
+            });
+
+            selectedItem.setAttribute('selected', true)
+            inputKodeBarang.value = kode
+            inputBarang.value = nama
+        </script>
+    </form>
 @endsection
--- aset-tetap/resources/views/Peminjaman/index.blade.php
+++ aset-tetap/resources/views/Peminjaman/index.blade.php
@@ -1,88 +1,129 @@
 @extends('dashboard.layouts.main')
 
 @section('container')
-<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-  <h1 class="h2">Peminjaman</h1>
-  <div class="btn-toolbar mb-2 mb-md-0">
-    <div class="btn-group me-2">
-      <a href="/Peminjaman/Cetak-Peminjaman" target="_blank" class="btn btn-sm btn-outline-secondary">PDF<span data-feather="printer"></span></a>
-      <a href="/Peminjaman/Export-Peminjaman" target="_blank" class="btn btn-sm btn-outline-secondary">Excel<span data-feather="printer"></span></a>
-    </div>
-  </div>
-</div>
-
-<div class="content">
-  <div class="card card-info card-outline">
-    <div class="card-header">
-      <div class="card-tools">
-        <a href="/Peminjaman/Create-Peminjaman" class="btn btn-success">Tambah Data<i class="fas fa-plus-square"></i></a>
-        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Import Data</button>
-      </div>
-    </div>
-
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Silahkan Import Data</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <form action="/Peminjaman/Import-Peminjaman" method="post" enctype="multipart/form-data">
-        @csrf
-      <div class="modal-body">
-        <div class="form-group">
-          <input type="file" name="file" required="required">
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">Peminjaman</h1>
+        <div class="btn-toolbar mb-2 mb-md-0">
+            <div class="btn-group me-2">
+                <a href="/Peminjaman/Cetak-Peminjaman" target="_blank" class="btn btn-sm btn-outline-secondary">PDF<span
+                        data-feather="printer"></span></a>
+                <a href="/Peminjaman/Export-Peminjaman" target="_blank" class="btn btn-sm btn-outline-secondary">Excel<span
+                        data-feather="printer"></span></a>
+            </div>
         </div>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Batal</button>
-        <button type="submit" class="btn btn-primary">Import Data</button>
-      </div>
     </div>
-    </form>
-  </div>
-</div>
 
-    <div class="card-body text-center">
-      <table class="table table-bordered">
-        <tr>
-          <th>Pihak Peminjam</th>
-          <th>Tanggal Peminjam</th>
-          <th>Kode Barang</th>
-          <th>Nama Barang</th>
-          <th>Tahun Perolehan</th>
-          <th>Cara Perolehan</th>
-          <th>jumlah barang</th>
-          <th>Harga</th>
-          <th>Kondisi Barang</th>
-          <th>Keterangan</th>
-          <th>Action</th>
-        </tr>
-        @foreach ($peminjaman as $item)
-        <tr>
-          <td>{{ $item->peminjam }}</td>
-          <td>{{ $item->tgl_pinjam }}</td>
-          <td>{{ $item->kode_barang }}</td>
-          <td>{{ $item->nama_barang }}</td>
-          <td>{{ $item->thn_perolehan }}</td>
-          <td>{{ $item->cara_perolehan }}</td>
-          <td>{{ $item->jmlh_barang }}</td>
-          <td>{{ $item->harga }}</td>
-          <td>{{ $item->kondisi_barang }}</td>
-          <td>{{ $item->ket }}</td>
-          <td>
-            <a href="{{ url('/Peminjaman/Edit-Peminjaman', $item->id) }}"><span data-feather="edit"></span></a>
-            | 
-            <a href="{{ url('/Peminjaman/delete-peminjaman', $item->id) }}"><span data-feather="trash-2" style="color: red"></span></a>
-          </td>
-        </tr>
-        @endforeach
-      </table>
+    <div class="content">
+        <div class="card card-info card-outline">
+            <div class="card-header">
+                <div class="card-tools">
+                    <a href="/Peminjaman/Create-Peminjaman" class="btn btn-success">Tambah Data<i
+                            class="fas fa-plus-square"></i></a>
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal"
+                        data-bs-target="#exampleModal">Import Data</button>
+                </div>
+            </div>
+
+            <!-- Modal -->
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+                aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Silahkan Import Data</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                aria-label="Close"></button>
+                        </div>
+                        <form action="/Peminjaman/Import-Peminjaman" method="post" enctype="multipart/form-data">
+                            @csrf
+                            <div class="modal-body">
+                                <div class="form-group">
+                                    <input type="file" name="file" required="required">
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Batal</button>
+                                <button type="submit" class="btn btn-primary">Import Data</button>
+                            </div>
+                    </div>
+                    </form>
+                </div>
+            </div>
+
+            <div class="card-body text-center">
+                <table class="table table-bordered">
+                    <tr>
+                        <th>Pihak Peminjam</th>
+                        <th>Tanggal Peminjam</th>
+                        <th>Kode Barang</th>
+                        <th>Nama Barang</th>
+                        <th>Tahun Perolehan</th>
+                        <th>Cara Perolehan</th>
+                        <th>Jumlah Barang</th>
+                        <th>Harga</th>
+                        <th>Kondisi Barang</th>
+                        <th>Keterangan</th>
+                        <th>Action</th>
+                    </tr>
+                    @foreach ($peminjaman as $item)
+                        <tr>
+                            <td>{{ $item->peminjam }}</td>
+                            <td>{{ $item->tgl_pinjam }}</td>
+                            <td>{{ $item->kode_barang }}</td>
+                            <td>{{ $item->nama_barang }}</td>
+                            <td>{{ $item->thn_perolehan }}</td>
+                            <td>{{ $item->cara_perolehan }}</td>
+                            <td>{{ $item->jmlh_barang }}</td>
+                            <td>Rp {{ $item->harga }}</td>
+                            <td>{{ $item->kondisi_barang }}</td>
+                            <td>{{ $item->ket }}</td>
+                            <td>
+                                <a href="{{ url('/Peminjaman/Edit-Peminjaman', $item->id) }}"><span
+                                        data-feather="edit"></span></a>
+                                |
+                                <a href="{{ url('/Peminjaman/delete-peminjaman', $item->id) }}"><span
+                                        data-feather="trash-2" style="color: red"></span></a>
+                            </td>
+                        </tr>
+                    @endforeach
+                    <tr class="fw-bold" style="background-color: #f7f7f7;">
+                      <td colspan="6">Total</td>
+                      <td id='total_jumlah'></td>
+                      <td id='total_harga'></td>
+                      <td colspan="3">-</td>
+                  </tr>
+                </table>
+            </div>
+        </div>
+
+        <script>
+            var peminjaman = {!! json_encode($peminjaman) !!};
+            var jumlah = document.getElementById("total_jumlah");
+            var harga = document.getElementById("total_harga");
+            var dataJumlah = [];
+            var dataHarga = [];
+
+            if (peminjaman.length > 0) {
+                for (let i = 0; i < peminjaman.length; i++) {
+                    dataJumlah.push(parseInt(peminjaman[i].jmlh_barang))
+                    dataHarga.push(parseInt(peminjaman[i].harga))
+                }
+            } else {
+                dataJumlah.push(0)
+                dataHarga.push(0)
+            }
+
+            const sumJumlah = dataJumlah.reduce(add, 0);
+            const sumHarga = dataHarga.reduce(add, 0);
+
+            function add(val, a) {
+                return val + a;
+            }
+
+            jumlah.innerHTML = sumJumlah
+            harga.innerHTML = `Rp ${sumHarga}`
+        </script>
+
+        @include('sweetalert::alert')
     </div>
-  </div>
-
-  @include('sweetalert::alert')
-</div>
-
 @endsection
